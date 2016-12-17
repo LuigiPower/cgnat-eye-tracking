c65b1d62e590157997ff355d157816ed12f43155
Federico Giuggioloni <federico.giuggioloni@gmail.com>
2016-12-17 19:52:28 +0100

    Eye tracking nearly done for left eye

--- track_eyes.m
+++ track_eyes.m
@@ -34,8 +34,8 @@ leftEye = SupportFunctions.getRightMost(leftEyes);
 rightEye = SupportFunctions.getLeftMost(rightEyes);
 
 %% Get Points to track in the video
-leftEyePupil = SupportFunctions.getCenter(leftEye);
-rightEyePupil = SupportFunctions.getCenter(rightEye);
+%leftEyePupil = SupportFunctions.getCenter(leftEye);
+%rightEyePupil = SupportFunctions.getCenter(rightEye);
 
 leftEyeImage = imcrop(videoFrame, leftEye);
 
@@ -50,6 +50,26 @@ leftEyeImage = imcrop(videoFrame, leftEye);
 %for i = 1:r
 %    leftEyeImage = insertObjectAnnotation(leftEyeImage, 'Circle', [centers(i, :) radii(i)], 'circle');
 %end
+
+%{
+%% RGB Normalization
+leftEyeImage_r = leftEyeImage(:, :, 1);
+leftEyeImage_g = leftEyeImage(:, :, 2);
+leftEyeImage_b = leftEyeImage(:, :, 3);
+
+normRed = leftEyeImage_r ./ sqrt(leftEyeImage_r .^ 2 + leftEyeImage_g .^ 2 + leftEyeImage_b .^ 2);
+normGreen = leftEyeImage_g ./ sqrt(leftEyeImage_r .^ 2 + leftEyeImage_g .^ 2 + leftEyeImage_b .^ 2);
+normBlue = leftEyeImage_b ./ sqrt(leftEyeImage_r .^ 2 + leftEyeImage_g .^ 2 + leftEyeImage_b .^ 2);
+
+leftEyeImageNormalized = cat(3, normRed, normGreen, normBlue);
+figure, imshow(normRed);
+figure, imshow(normGreen);
+figure, imshow(normBlue);
+figure, imshow(leftEyeImageNormalized);
+%}
+
+%{
+%% mask contour segmentation
 leftEyeImage = histeq(rgb2gray(leftEyeImage));
 figure, imshow(leftEyeImage);
 mask = zeros(size(leftEyeImage));
@@ -62,8 +82,8 @@ bw = activecontour(leftEyeImage, mask, 300);
 
 figure, imshow(bw);
 title('Segmented Image');
+%}
 
-%{
 %% Clustering
 cform = makecform('srgb2lab');
 
@@ -80,15 +100,66 @@ nrows = size(ab, 1);
 ncols = size(ab, 2);
 ab = reshape(ab, nrows * ncols, 2);
 
-nColors = 2;
+nColors = 10;
 % repeat the clustering 3 times to avoid local minima
 [cluster_idx, cluster_center] = kmeans(ab, nColors, 'distance', 'sqEuclidean', ...
                                       'Replicates', 3);
-                                  
+
 pixel_labels = reshape(cluster_idx, nrows, ncols);
 
-imshow(pixel_labels, []), title('image labeled by cluster index');
-%}
+[n, m] = size(pixel_labels);
+centers = zeros(10, 2);
+counts = zeros(10);
+cluster_images = zeros(n, m, 10);
+cluster_images(1:n, 1:m, 1:10) = 0;
+
+for cluster = 1:10
+    for i = 1:n
+        for j = 1:m
+            if(pixel_labels(i, j) == cluster)
+                centers(cluster, :) = centers(cluster, :) + [i, j];
+                counts(cluster) = counts(cluster) + 1;
+                cluster_images(i, j, cluster) = 255;
+            end
+        end
+    end
+    centers(cluster, :) = centers(cluster, :) ./ counts(cluster);
+end
+
+figure, imshow(pixel_labels, []), title('image labeled by cluster index');
+
+radiis = [];
+maxcircle = [0 0];
+maxradii = 0.0;
+maxcluster = 1;
+
+leftEyeImageGSShow = leftEyeImageGS;
+for i = 1:10
+    leftEyeImageGSShow = insertMarker(leftEyeImageGSShow, [centers(i, 2) centers(i, 1)], '+', 'Color', 'red');
+    
+    
+    [circleCenters, radii, metric] = imfindcircles(cluster_images(:, :, i), [1 m]);
+    [nc, mc] = size(circleCenters);
+    if(nc > 0)
+        %figure; imshow(cluster_images(:, :, i)); title('Detected face');
+        %viscircles(circleCenters, radii, 'EdgeColor', 'r');
+        radiis = [radiis; radii];
+        [mradius, argmradius] = max(radii);
+        if(mradius > maxradii)
+            maxcluster = i;
+            maxradii = max(radii);
+            maxcircle = circleCenters(argmradius, :);
+        end
+    end
+end
+
+figure; imshow(cluster_images(:, :, maxcluster)); title('MAXCLUSTER');
+viscircles(maxcircle, maxradii, 'EdgeColor', 'r');
+
+leftEyeImageGSShow = insertMarker(leftEyeImageGSShow, [m/2 n/2], '+', 'Color', 'green');
+figure; imshow(leftEyeImageGSShow); title('Clustered');
+
+leftEyePupil = leftEye(1, 1:2) + maxcircle;
 
 %% Draw the returned bounding box around the detected face.
 videoFrameShow = insertObjectAnnotation(videoFrame, 'Rectangle', face_of_interest, 'Face');
@@ -104,10 +175,10 @@ videoFrameShow = insertObjectAnnotation(videoFrameShow, 'Rectangle', nose, 'Nose
 %end
 
 videoFrameShow = insertMarker(videoFrameShow, leftEyePupil, '+', 'Color', 'red');
-videoFrameShow = insertMarker(videoFrameShow, rightEyePupil, '+', 'Color', 'red');
+%videoFrameShow = insertMarker(videoFrameShow, rightEyePupil, '+', 'Color', 'red');
 
 % Show The first frame, with all found features
-%figure; imshow(videoFrameShow); title('Detected face');
+figure; imshow(videoFrameShow); title('Detected face');
 
 %% Video tracking
 % Create a point tracker and enable the bidirectional error constraint to
@@ -116,13 +187,16 @@ pointTracker = vision.PointTracker('MaxBidirectionalError', 2);
 
 %points = [leftEyePupil; rightEyePupil];
 greyscaleVideoFrame = rgb2gray(videoFrame);
-leftPoints = detectMinEigenFeatures(greyscaleVideoFrame, 'ROI', leftEye);
-rightPoints = detectMinEigenFeatures(greyscaleVideoFrame, 'ROI', rightEye);
+%leftPoints = detectMinEigenFeatures(greyscaleVideoFrame, 'ROI', leftEye);
+%rightPoints = detectMinEigenFeatures(greyscaleVideoFrame, 'ROI', rightEye);
 %leftPoints = detectHarrisFeatures(rgb2gray(videoFrame), 'ROI', leftEye);
 %rightPoints = detectHarrisFeatures(rgb2gray(videoFrame), 'ROI', rightEye);
 %imshow(greyscaleVideoFrame);
 
-points = [leftPoints.Location; rightPoints.Location];
+leftIrisLeft = [leftEyePupil(1) - maxradii leftEyePupil(2)];
+leftIrisRight = [leftEyePupil(1) + maxradii leftEyePupil(2)];
+
+points = [leftEyePupil; leftIrisLeft; leftIrisRight];
 oldPoints = points;
 
 initialize(pointTracker, points, videoFrame);
@@ -134,7 +208,7 @@ videoPlayer  = vision.VideoPlayer('Position',...
 % This is needed to be able to visualize the rotation of the object.
 bboxPointsLeft = bbox2points(leftEye);
 bboxPointsRight = bbox2points(rightEye);
-%{
+
 while ~isDone(videoFileReader)
     % get the next frame
     videoFrame = step(videoFileReader);
@@ -178,4 +252,4 @@ while ~isDone(videoFileReader)
     % Display the annotated video frame using the video player object
     step(videoPlayer, videoFrame);
 end
-%}
+
