8b21e10a2c3907dd15481adfadc3415b48997750
Federico Giuggioloni <federico.giuggioloni@gmail.com>
2016-12-17 15:18:47 +0100

    edits

---
+++ SupportFunctions.m
@@ -0,0 +1,77 @@
+classdef SupportFunctions
+    methods (Static)
+        function[maxarea, maxindex] = orderDescByArea(bboxes)
+            [n, ~] = size(bboxes);
+            prods = zeros(n);
+            for i = 1:n
+                prods(i) = prod(bboxes(i, 3:4));
+            end
+            [maxarea, maxindex] = max(prods);
+        end
+        
+        function[maxintersect, i, j] = getMaxIntersect(bboxes1, bboxes2)
+            [n1, ~] = size(bboxes1);
+            [n2, ~] = size(bboxes2);
+            intersects = zeros(n1, n2);
+            for i = 1:n1
+                for j = 1:n2
+                    intersects(i, j) = bboxOverlapRatio(bboxes1(i, :), bboxes2(j, :));
+                end
+            end
+            [intersects2, index] = max(intersects);
+            [maxintersect, j] = max(intersects2);
+            i = index(j);
+        end
+        
+        %% removeNonIntersecting
+        % Removes all non intersecting bounding boxes from first argument
+        function[boxes] = removeNonIntersecting(bboxes1, bboxes2)
+            [n1, m1] = size(bboxes1);
+            [n2, ~] = size(bboxes2);
+            boxes = int16.empty(0, m1);
+            for i = 1:n1
+                for j = 1:n2
+                    intersection = bboxOverlapRatio(bboxes1(i, :), bboxes2(j, :));
+                    if(intersection > 0)
+                        boxes = [boxes;bboxes1(i, :)];
+                        %boxes(top, :) = bboxes1(i, :);
+                    end
+                end
+            end
+        end
+        
+        %% getLeftMost
+        % Get left most bounding box
+        function[box] = getLeftMost(bboxes)
+            [n1, ~] = size(bboxes);
+            box = bboxes(1, :);
+            minimum = box(1, 1);
+            for i = 1:n1
+                if(bboxes(i, 1) < minimum)
+                    box = bboxes(i, :);
+                    box
+                end
+            end
+        end
+        
+        %% getRightMost
+        % Get right most bounding box
+        function[box] = getRightMost(bboxes)
+            [n1, ~] = size(bboxes);
+            box = bboxes(1, :);
+            maximum = box(1, 1);
+            for i = 1:n1
+                if(bboxes(i, 1) > maximum)
+                    box = bboxes(i, :);
+                    box
+                end
+            end
+        end
+        
+        %% getCenter
+        % get the center point of the bounding box
+        function[point] = getCenter(bbox)
+            point = [(bbox(1, 1) + bbox(1, 3) / 2) (bbox(1, 2) + bbox(1, 4) / 2)];
+        end
+    end
+end
--- track_eyes.m
+++ track_eyes.m
@@ -1,13 +1,47 @@
 clear; close all; clc;
 
-faceDetector = vision.CascadeObjectDetector();
-
 %% Load Video file
 videoFileReader = vision.VideoFileReader('mds_project_xxx.mov');
 videoFrame      = step(videoFileReader);
-bbox            = faceDetector(videoFrame);
 
-% Draw the returned bounding box around the detected face.
-videoFrame = insertShape(videoFrame, 'Rectangle', bbox);
+faceDetector = vision.CascadeObjectDetector();
+eyeDetector = vision.CascadeObjectDetector('EyePairSmall', 'UseROI', true);
+leftEyeDetector = vision.CascadeObjectDetector('LeftEye', 'UseROI', true);
+rightEyeDetector = vision.CascadeObjectDetector('RightEye', 'UseROI', true);
+noseDetector = vision.CascadeObjectDetector('Nose', 'UseROI', true);
+
+bbox = faceDetector(videoFrame);
+[maximum, indexes] = SupportFunctions.orderDescByArea(bbox);
+
+face_of_interest = bbox(indexes(1), :);
+eyes = eyeDetector(videoFrame, face_of_interest);
+nose = noseDetector(videoFrame, face_of_interest);
+
+leftEyes = leftEyeDetector(videoFrame, face_of_interest);
+rightEyes = rightEyeDetector(videoFrame, face_of_interest);
+
+%% Need some processing to find the correct Left Eye and Right Eye
+% by using the "eyes" Bounding Box, and then picking the best box
+leftEyes = SupportFunctions.removeNonIntersecting(leftEyes, eyes);
+rightEyes = SupportFunctions.removeNonIntersecting(rightEyes, eyes);
+
+leftEye = SupportFunctions.getRightMost(leftEyes);
+rightEye = SupportFunctions.getLeftMost(rightEyes);
+
+%% Get Points to track in the video
+leftEyePupil = SupportFunctions.getCenter(leftEye);
+rightEyePupil = SupportFunctions.getCenter(rightEye);
+
+%% Draw the returned bounding box around the detected face.
+videoFrame = insertObjectAnnotation(videoFrame, 'Rectangle', face_of_interest, 'Face');
+videoFrame = insertObjectAnnotation(videoFrame, 'Rectangle', eyes, 'Eyes');
+videoFrame = insertObjectAnnotation(videoFrame, 'Rectangle', leftEye, 'Left Eye');
+videoFrame = insertObjectAnnotation(videoFrame, 'Rectangle', rightEye, 'Right Eye');
+videoFrame = insertObjectAnnotation(videoFrame, 'Rectangle', nose, 'Nose');
+
+videoFrame = insertMarker(videoFrame, leftEyePupil, '+', 'Color', 'red');
+videoFrame = insertMarker(videoFrame, rightEyePupil, '+', 'Color', 'red');
+
+% Show The first frame, with all found features
 figure; imshow(videoFrame); title('Detected face');
 
