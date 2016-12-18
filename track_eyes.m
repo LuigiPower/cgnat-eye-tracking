725a56aca9a7d310f8d7ba4ae75003d0016e1a81
Federico Giuggioloni <federico.giuggioloni@gmail.com>
2016-12-18 16:25:28 +0100

    Refactoring

--- track_eyes.m
+++ track_eyes.m
@@ -35,134 +35,9 @@ end
 leftEye = SupportFunctions.getRightMost(leftEyes);
 rightEye = SupportFunctions.getLeftMost(rightEyes);
 
-%% Get Points to track in the video
-%leftEyePupil = SupportFunctions.getCenter(leftEye);
-%rightEyePupil = SupportFunctions.getCenter(rightEye);
-
-leftEyeImage = imcrop(videoFrame, leftEye);
-
-%videoFrame(leftEye(1, 2):(leftEye(1, 2) + leftEye(1, 4)), ...
-%            leftEye(1, 1):(leftEye(1, 1) + leftEye(1, 3))), ...
-
-%% Left Eye analysis
-[w, h] = size(leftEyeImage);
-%[centers, radii, metric] = imfindcircles(leftEyeImage, h/3);
-
-%[r, ~] = size(centers);
-%for i = 1:r
-%    leftEyeImage = insertObjectAnnotation(leftEyeImage, 'Circle', [centers(i, :) radii(i)], 'circle');
-%end
-
-%{
-%% RGB Normalization
-leftEyeImage_r = leftEyeImage(:, :, 1);
-leftEyeImage_g = leftEyeImage(:, :, 2);
-leftEyeImage_b = leftEyeImage(:, :, 3);
-
-normRed = leftEyeImage_r ./ sqrt(leftEyeImage_r .^ 2 + leftEyeImage_g .^ 2 + leftEyeImage_b .^ 2);
-normGreen = leftEyeImage_g ./ sqrt(leftEyeImage_r .^ 2 + leftEyeImage_g .^ 2 + leftEyeImage_b .^ 2);
-normBlue = leftEyeImage_b ./ sqrt(leftEyeImage_r .^ 2 + leftEyeImage_g .^ 2 + leftEyeImage_b .^ 2);
-
-leftEyeImageNormalized = cat(3, normRed, normGreen, normBlue);
-figure, imshow(normRed);
-figure, imshow(normGreen);
-figure, imshow(normBlue);
-figure, imshow(leftEyeImageNormalized);
-%}
-
-%{
-%% mask contour segmentation
-leftEyeImage = histeq(rgb2gray(leftEyeImage));
-figure, imshow(leftEyeImage);
-mask = zeros(size(leftEyeImage));
-mask(25:end-25,25:end-25) = 1;
-
-figure, imshow(mask);
-title('Initial Contour Location');
-
-bw = activecontour(leftEyeImage, mask, 300);
-
-figure, imshow(bw);
-title('Segmented Image');
-%}
-
-%% Clustering
-cform = makecform('srgb2lab');
-
-leftEyeImageGS = rgb2gray(leftEyeImage);
-leftEyeImage(:, :, 1) = leftEyeImageGS;
-leftEyeImage(:, :, 2) = leftEyeImageGS;
-leftEyeImage(:, :, 3) = leftEyeImageGS;
-%imshow(leftEyeImage);
-
-lab_leftEyeImage = applycform(double(leftEyeImage), cform);
-
-ab = double(lab_leftEyeImage(:, :, 2:3));
-nrows = size(ab, 1);
-ncols = size(ab, 2);
-ab = reshape(ab, nrows * ncols, 2);
-
-nColors = 10;
-% repeat the clustering 3 times to avoid local minima
-[cluster_idx, cluster_center] = kmeans(ab, nColors, 'distance', 'sqEuclidean', ...
-                                      'Replicates', 3);
-
-pixel_labels = reshape(cluster_idx, nrows, ncols);
-
-[n, m] = size(pixel_labels);
-centers = zeros(10, 2);
-counts = zeros(10);
-cluster_images = zeros(n, m, 10);
-cluster_images(1:n, 1:m, 1:10) = 0;
-
-for cluster = 1:10
-    for i = 1:n
-        for j = 1:m
-            if(pixel_labels(i, j) == cluster)
-                centers(cluster, :) = centers(cluster, :) + [i, j];
-                counts(cluster) = counts(cluster) + 1;
-                cluster_images(i, j, cluster) = 255;
-            end
-        end
-    end
-    centers(cluster, :) = centers(cluster, :) ./ counts(cluster);
-end
-
-figure, imshow(pixel_labels, []), title('image labeled by cluster index');
-
-radiis = [];
-maxcircle = [0 0];
-maxradii = 0;
-maxcluster = 1;
-
-leftEyeImageGSShow = leftEyeImageGS;
-for i = 1:10
-    leftEyeImageGSShow = insertMarker(leftEyeImageGSShow, [centers(i, 2) centers(i, 1)], '+', 'Color', 'red');
-    
-    
-    [circleCenters, radii, metric] = imfindcircles(cluster_images(:, :, i), uint16([m/18 m/6]));
-    [nc, mc] = size(circleCenters);
-    if(nc > 0)
-        %figure; imshow(cluster_images(:, :, i)); title('Detected face');
-        %viscircles(circleCenters, radii, 'EdgeColor', 'r');
-        radiis = [radiis; radii];
-        [mradius, argmradius] = max(radii);
-        if(mradius > maxradii)
-            maxcluster = i;
-            maxradii = max(radii);
-            maxcircle = circleCenters(argmradius, :);
-        end
-    end
-end
-
-figure; imshow(cluster_images(:, :, maxcluster)); title('MAXCLUSTER');
-viscircles(maxcircle, maxradii, 'EdgeColor', 'r');
-
-leftEyeImageGSShow = insertMarker(leftEyeImageGSShow, [m/2 n/2], '+', 'Color', 'green');
-figure; imshow(leftEyeImageGSShow); title('Clustered');
-
-leftEye = double(leftEye);
-leftEyePupil = leftEye(1, 1:2) + maxcircle;
+%% Eye finding
+[leftEyePupil, leftIris] = DetectionHelper.findEye(videoFrame, leftEye, 6);
+[rightEyePupil, rightIris] = DetectionHelper.findEye(videoFrame, rightEye, 6);
 
 %% Draw the returned bounding box around the detected face.
 videoFrameShow = insertObjectAnnotation(videoFrame, 'Rectangle', face_of_interest, 'Face');
@@ -172,13 +47,8 @@ videoFrameShow = insertObjectAnnotation(videoFrameShow, 'Rectangle', leftEye, 'L
 videoFrameShow = insertObjectAnnotation(videoFrameShow, 'Rectangle', rightEye, 'Right Eye');
 videoFrameShow = insertObjectAnnotation(videoFrameShow, 'Rectangle', nose, 'Nose');
 
-%[r, ~] = size(centers);
-%for i = 1:r
-%    videoFrameShow = insertObjectAnnotation(videoFrameShow, 'Circle', [(centers(i) + leftEye(1, 1:2)) radii(i)], 'circle');
-%end
-
 videoFrameShow = insertMarker(videoFrameShow, leftEyePupil, '+', 'Color', 'red');
-%videoFrameShow = insertMarker(videoFrameShow, rightEyePupil, '+', 'Color', 'red');
+videoFrameShow = insertMarker(videoFrameShow, rightEyePupil, '+', 'Color', 'red');
 
 % Show The first frame, with all found features
 figure; imshow(videoFrameShow); title('Detected face');
@@ -196,10 +66,8 @@ greyscaleVideoFrame = rgb2gray(videoFrame);
 %rightPoints = detectHarrisFeatures(rgb2gray(videoFrame), 'ROI', rightEye);
 %imshow(greyscaleVideoFrame);
 
-leftIrisLeft = [leftEyePupil(1) - maxradii leftEyePupil(2)];
-leftIrisRight = [leftEyePupil(1) + maxradii leftEyePupil(2)];
 
-points = [leftEyePupil; leftIrisLeft; leftIrisRight];
+points = [leftEyePupil; leftIris; rightEyePupil; rightIris];
 oldPoints = points;
 
 initialize(pointTracker, points, videoFrame);
