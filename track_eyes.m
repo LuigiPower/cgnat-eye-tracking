9fa705e73c51b661f62d49cb4e15559ef00dfec2
Federico Giuggioloni <federico.giuggioloni@gmail.com>
2017-07-06 17:03:10 +0200

    waitforbuttonpress

--- track_eyes.m
+++ track_eyes.m
@@ -312,6 +312,14 @@ while ~isDone(videoFileReader)
     setPoints(pointTracker, oldPoints);
     %end
     
+    f = figure;
+    w = waitforbuttonpress;
+    if w == 0
+        disp('Button click')
+    else
+        disp('Key press')
+    end
+
     % Display the annotated video frame using the video player object
     step(videoPlayer, videoFrame);
 end
