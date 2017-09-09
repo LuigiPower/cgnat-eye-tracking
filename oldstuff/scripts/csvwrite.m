5ed2300d1fca5d0d612635edb72a6afead46eabf
Federico Giuggioloni <federico.giuggioloni@gmail.com>
2017-09-09 22:11:21 +0200

    removed videos from git

--- oldstuff/scripts/csvwrite.m
+++
@@ -1,6 +0,0 @@
-csvwrite('badcg_complete_xleft.csv', xLeftEyeCenter);
-csvwrite('badcg_complete_yleft.csv', yLeftEyeCenter);
-csvwrite('badcg_complete_yright.csv', yRightEyeCenter);
-csvwrite('badcg_complete_xright.csv', xRightEyeCenter);
-csvwrite('badcg_complete_xdiff.csv', xDiffCenter);
-csvwrite('badcg_complete_ydiff.csv', yDiffCenter);
